74448254ad64548546425b5de7f7f9593e94ee3d
wenjie.geng <gwjlib@163.com>
2019-12-02 16:37:38 +0800

    自动生成目录结构路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
+const pagesContext = require.context(
+  '@/pages',
+  false,
+  /\.vue$/
+)
+
+let routes = []
+
+pagesContext.keys().forEach(fileName => {
+  fileName = fileName.replace(/\.\/([\w\W\d_]+)\.vue$/, (s, $1) => $1)
+
+  routes.push({
+    path: `/${fileName}`,
+    name: fileName,
+    component: () => import(`@/pages/${fileName}`)
+  })
+})
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes
 })
